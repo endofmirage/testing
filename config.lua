61238846d11ac12426a02664afff29bce5f737d3
endofmirage <endofmirage@protonmail.com>
2026-08-14 00:14:48 -0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,17 @@
+return {
+
+    Users = {
+
+        [4708859865] = {
+            Username = "ritualsOT",
+            Role = "Owner"
+        },
+
+        [123456789] = {
+            Username = "Friend",
+            Role = "Tester"
+        }
+
+    }
+
+}
