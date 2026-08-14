cace740c00b48650941a8c4754c186416664d039
endofmirage <endofmirage@protonmail.com>
2026-08-14 00:54:37 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,14 +2,39 @@ return {
 
     Users = {
 
-        [4708859865] = {
-            Username = "ritualsOT",
-            Role = "Owner"
+        [10453270069] = {
+            Username = "xantrithorss",
+            Role = "Developer"
         },
 
-        [10453219812] = {
-            Username = "xyrandsu",
+        [8724292651] = {
+            Username = "korvannsc",
+            Role = "Developer"
+        },
+
+        [8724295489] = {
+            Username = "holdwarden",
+            Role = "Developer"
+        },
+
+        [1700820632] = {
+            Username = "Sheluvdev",
             Role = "Tester"
+        },
+
+        [10718744298] = {
+            Username = "Qasentyz",
+            Role = "Tester"
+        },
+
+        [319622251] = {
+            Username = "gamerwithfakedeion",
+            Role = "Tester"
+        },
+
+        [3756708106] = {
+            Username = "XxGoldenWariorxX1202",
+            Role = "Bald Headed Ass Nigga"
         }
 
     }
