e1e1e657d482c84bd06efa3c4b278f2e90bade4c
endofmirage <endofmirage@protonmail.com>
2026-08-14 17:27:14 -0700

    Create xx.lua

---
+++ xx.lua
@@ -0,0 +1,12 @@
+{
+    "keys": {
+        "flipurs-xakhsh-asjbba-asbkhy": {
+            "expires": "9999-12-31",
+            "role": "Developer"
+        },
+        "VIP456": {
+            "expires": "2026-12-31",
+            "role": "VIP"
+        }
+    }
+}
